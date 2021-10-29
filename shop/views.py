eb85a951417feb61bbefa1eb3f05448e48d8173f
Viktoria <viktrols@yandex.ru>
2021-10-29 17:02:08 +0300

    add img for readme

--- shop/views.py
+++ shop/views.py
@@ -50,7 +50,7 @@ def _cart_id(request):
     cart = request.session.session_key
     if not cart:
         cart = request.session.create()
-	return cart
+    return cart
 
 
 def add_cart(request, product_id):
